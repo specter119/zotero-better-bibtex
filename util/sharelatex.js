0b523963d22b5e5c5905261b9e4b9fb8f1e3994f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-03-22 13:23:03 +0100

    This was a triumph! I'm making a note here: Huge success!
    It's hard to overstate
    my satisfaction.
    
    Better BibTeX:
    We do what we must
    because we can
    For the good of all of us.

--- util/sharelatex.js
+++ util/sharelatex.js
@@ -1,177 +1,140 @@
-var DOMParser = Components.classes['@mozilla.org/xmlextras/domparser;1'].createInstance(Components.interfaces.nsIDOMParser);
-
-var loginPage;
-var _csrf;
-var chatter = [];
-
 try {
-  loginPage = await fetch('https://www.sharelatex.com/login', {
-    cache: 'no-cache',
-    credentials: 'include',
-    method: 'GET',
-    mode: 'same-origin',
-    redirect: 'follow',
-    // referrer: 'no-referrer',
-  });
+  var chatter = [];
+  var DOMParser = Components.classes['@mozilla.org/xmlextras/domparser;1'].createInstance(Components.interfaces.nsIDOMParser);
 
-  var doc = DOMParser.parseFromString(await loginPage.text(), 'text/html');
-  _csrf = Array.from(doc.getElementsByTagName('input')).filter(input => input.getAttribute('name') === '_csrf').map(input => input.getAttribute('value')).join(',');
-} catch (err) {
-  return `could not get login page: ${err.message}, ${JSON.stringify(err)}`;
-}
-
-function encode(params) {
-  var encoded = []
-  for (const [key, value] of Object.entries(params)) {
-    encoded.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
-  return encoded.join('&').replace(/%20/g, '+');
-}
 
-try {
-  loginPage = await fetch('https://www.sharelatex.com/login', {
+  function encode(params) {
+    var encoded = []
+    for (const [key, value] of Object.entries(params)) {
+      encoded.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    }
+    return encoded.join('&').replace(/%20/g, '+');
+  }
+
+  async function _fetch(url, options) {
+    if (!options) options = {};
+    if (!options.method) options.method = 'GET';
+
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open(options.method, url);
+
+      if (options.headers) {
+        for (const [key, value] of Object.entries(options.headers)) {
+          req.setRequestHeader(key, value);
+        }
+      }
+
+      req.onload = function() {
+        if (req.status == 200) {
+          resolve(req);
+        }
+        else {
+          reject(Error(req.statusText));
+        }
+      };
+
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+
+      req.send(options.body);
+    });
+  }
+
+  async function expect(channel, cookie, expected, data) {
+    var poll = await _fetch(`https://www.sharelatex.com/socket.io/1/xhr-polling/${channel}?t=${Date.now().toString()}`, {
+      headers: { Cookie: cookie },
+      method: data ? 'POST' : 'GET',
+      body: data,
+    })
+    var m = poll.response.match(expected);
+    if (!m) throw new Error(`expected ${expected}, got ${poll.response}`);
+    return m;
+  }
+
+  var page = await _fetch('https://www.sharelatex.com/login');
+  var doc = DOMParser.parseFromString(page.response, 'text/html');
+  var _csrf = Array.from(doc.getElementsByTagName('input')).find(input => input.getAttribute('name') === '_csrf').getAttribute('value');
+
+  var page = await _fetch('https://www.sharelatex.com/login', {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded"
+    },
     body: encode({
       email: Zotero.Prefs.get('translators.better-bibtex.ShareLaTeX_email'),
       password: Zotero.Prefs.get('translators.better-bibtex.ShareLaTeX_password'),
       _csrf: _csrf,
       redir: '',
     }),
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'include', // include, same-origin, *omit
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-    },
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, cors, *same-origin
-    redirect: 'follow', // *manual, follow, error
-    referrer: 'no-referrer', // *client, no-referrer
   });
-} catch (err) {
-  return `could not login: ${err.message} ${JSON.stringify(err)}`;
-}
-var loggedIn = await loginPage.json();
+  var cookie = page.getResponseHeader('Set-Cookie');
+  var response = JSON.parse(page.response);
 
-if (loggedIn.redir !== '/project') throw new Error('not authenticated');
+  if (response.redir !== '/project') throw new Error(`unexpected response: ${page.response}`);
 
-var projectPage;
-var projects = null;
-try {
-  projectPage = await fetch('https://www.sharelatex.com/project', {
-    cache: 'no-cache',
-    credentials: 'include',
-    method: 'GET',
-    mode: 'same-origin',
-    redirect: 'follow',
-    referrer: 'no-referrer',
-  });
-
-  var doc = DOMParser.parseFromString(await projectPage.text(), 'text/html')
-  projects = JSON.parse(doc.getElementById('data').textContent).projects;
-} catch (err) {
-  return `could not get projects: ${err.message} ${JSON.stringify(err)}`;
-}
-
-var timestamp = Date.now().toString()
-var channelInfo = await fetch(`https://www.sharelatex.com/socket.io/1/?t=${timestamp}`)
-var channel = (await channelInfo.text()).split(':')[0]
-
-var project = projects.find(project => project.name === 'MWE');
-
-/*
-function folderId() {
-  return new Promise(function(resolve, reject) {
-    var server = new WebSocket(`wss://www.sharelatex.com/socket.io/1/websocket/${channel}`);
-    var start = Date.now();
-
-    server.onopen = function() {
-      chatter.push([Date.now() - start, 'opened']);
-      setTimeout(function() {
-        chatter.push([Date.now() - start, 'done']);
-        resolve('done');
-      }, 2000);
-    };
-
-    server.onmessage = function(event) {
-      chatter.push([Date.now() - start, event.data]);
-    };
-
-    server.onerror = function(err) {
-      chatter.push([Date.now() - start, 'err', err]);
-      reject(err);
-    };
-
-    server.onclose = function(event) {
-      chatter.push([Date.now() - start, 'closed', event.code]);
-    };
+  var page = await _fetch('https://www.sharelatex.com/project', {
+    headers: { Cookie: cookie }
   })
-}
+  var doc = DOMParser.parseFromString(page.response, 'text/html');
+  var projects = JSON.parse(doc.getElementById('data').textContent).projects;
 
-await folderId();
-*/
+  var project = projects.find(project => project.name === 'MWE');
 
-async function poll() {
-  var url = `https://www.sharelatex.com/socket.io/1/xhr-polling/${channel}?t=${Date.now().toString()}`;
-  chatter.push(url);
-  var response = await fetch(url, {
-    cache: 'no-cache',
-    credentials: 'include',
-    method: 'GET',
-    // mode: 'cors',
-    // redirect: 'follow',
-    // referrer: 'no-referrer',
+  var timestamp = Date.now().toString()
+  var channel = await _fetch(`https://www.sharelatex.com/socket.io/1/?t=${timestamp}`, {
+    headers: { Cookie: cookie }
   });
-  return await response.text();
-}
+  channel = channel.response.split(':')[0];
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+  await expect(channel, cookie, /^1::$/);
 
-try {
-  chatter.push(await poll());
+  var accepted = await expect(channel, cookie, /^5:::({.+})$/);
+  if (JSON.parse(accepted[1]).name !== 'connectionAccepted') throw new Error(`connected rejected: ${accepted.toString()}`);
+
   await sleep(200);
-  chatter.push(await poll());
+
+  var join = await expect(channel, cookie, /^1$/, `5:1+::${JSON.stringify({name: 'joinProject', args: [{ project_id: project.id }]})}`)
+
   await sleep(200);
-  chatter.push(await poll());
-  await sleep(200);
-} catch (err) {
-  return `could not poll: ${err.message} ${JSON.stringify(err)}`;
-}
 
-return chatter;
+  project = await expect(channel, cookie, /^6:::1\+(\[.+\])$/);
+  project = JSON.parse(project[1])[1];
+  project = {
+    id: project._id,
+    root: project.rootFolder[0]._id,
+    docs: project.rootFolder[0].docs.reduce((docs, doc) => { docs[doc._id] = doc.name; return docs }, {}),
+  }
 
-/*
-var body = new FormData();
-try {
-  body.append('qqfile', new Blob(['emiliano']), 'better-bibtex.bib')
-} catch (err) {
-  return `could not create formdata: ${err.message} ${JSON.stringify(err)}`;
-}
+  // 5aad1630575dbf74c7116da5
 
-var params = encode({
-  // folder_id = ?
-  __csrf: _csrf,
-  qquuid: `${project.id}-better-bibtex`,
-  qqfilename: 'better-bibtex.bib',
-  qqtotalfilesize: 8,
-});
+  var bib = 'heyns';
+  var filename = 'better-bibtex.bib';
 
-try {
-  loginPage = await fetch(`https://www.sharelatex.com/project/${project.id}/upload?${params}`, {
+  var body = new FormData();
+  body.append('qqfile', new Blob([bib]), filename);
+
+  var params = encode({
+    folder_id: project.root,
+    _csrf: _csrf,
+    qquuid: `${project.id}-${filename}`,
+    qqfilename: filename,
+    qqtotalfilesize: bib.length,
+  });
+
+  var upload = await _fetch(`https://www.sharelatex.com/project/${project.id}/upload?${params}`, {
+    headers: { Cookie: cookie },
     body: body,
-    cache: 'no-cache',
-    credentials: 'include',
-    headers: {
-      'content-type': 'multipart/form-data',
-    },
     method: 'POST',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referrer',
-  });
-} catch (err) {
-  return `could not post bibfile: ${err.message} ${JSON.stringify(err)}`;
-}
+  })
+  if (!JSON.parse(upload.response).success) throw new Error(`upload: expected success, got ${upload.response}`);
 
-return loginPage.statusText
-*/
+  return upload.response;
+
+} catch (err) {
+  return `Oops; ${err.message}: ${err.stack}`;
+}
